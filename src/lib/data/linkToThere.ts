f255e928b389e7a630016aca3f7dbfe3cbfa9f2c
Joel K Rivers <48956906+lanternLightDeveloper@users.noreply.github.com>
2025-09-22 13:13:13 -0700

    Add files via upload

---
+++ src/lib/data/linkToThere.ts
@@ -0,0 +1,66 @@
+export type LinkToThere = {
+	label: string;
+	href: string;
+	img: string;
+};
+
+import imgRodeo from '$lib/imgs/rodeo.jpg';
+import imgEntertainment from '$lib/imgs/entertainment.jpg';
+import imgCarnival from '$lib/imgs/carnival.jpg';
+import imgAuction from '$lib/imgs/auction.jpg';
+import imgFood from '$lib/imgs/food.jpg';
+import imgCommercial from '$lib/imgs/commercial.jpg';
+import imgSubmit from '$lib/imgs/submit.jpg';
+
+export const ExhibitorForms = [
+	{
+		label: 'Art',
+		href: 'https://www.kitsapfair.org/_files/ugd/d66194_375ba46f43a842e1a928a21bf6438434.pdf',
+		img: imgRodeo
+	},
+	{
+		label: 'Hobbies',
+		href: 'https://www.kitsapfair.org/_files/ugd/d66194_e99d3d6d2f5445f0aa941b7102181ff6.pdf',
+		img: imgEntertainment
+	},
+	{
+		label: 'Photography',
+		href: 'https://www.kitsapfair.org/_files/ugd/d66194_04948534452b42dda0f687e9ba221bc9.pdf',
+		img: imgCarnival
+	},
+	{
+		label: 'Home Arts',
+		href: 'https://www.kitsapfair.org/_files/ugd/d66194_906ed316351745aa8604ba1a2d2bba61.pdf',
+		img: imgAuction
+	},
+	{
+		label: 'Honey',
+		href: ' https://www.kitsapfair.org/_files/ugd/d66194_6e2bec3ecaf546c885d330184cfd54fa.pdf',
+		img: imgFood
+	},
+	{
+		label: 'Floral',
+		href: '/https://www.kitsapfair.org/_files/ugd/d66194_3300318e63f64503a919939877629785.pdf',
+		img: imgCommercial
+	},
+	{
+		label: 'Flower design',
+		href: 'https://www.kitsapfair.org/_files/ugd/d66194_39c20c9a5cf24234bc0319966f3fc24d.pdf',
+		img: imgSubmit
+	},
+	{
+		label: 'Horticulture',
+		href: 'https://www.kitsapfair.org/_files/ugd/d66194_23ab044593f240b0aaf60af45db6b06a.pdf',
+		img: imgSubmit
+	},
+	{
+		label: 'Baking',
+		href: 'https://www.kitsapfair.org/_files/ugd/d66194_ce4939831fd54ff9ac8c3d93d63d1986.pdf',
+		img: imgSubmit
+	},
+	{
+		label: 'Horticulture',
+		href: 'https://www.kitsapfair.org/_files/ugd/d66194_02f4b3fd6ffc421483e17d691dbc786a.pdf',
+		img: imgSubmit
+	}
+];
